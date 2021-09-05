1f8a4db6ed411631c4a168b29942e7852f868b97
AlexBotya <alex_balg@bk.ru>
2021-09-05 23:42:58 +0300

    Lesson3. continue working

--- src/main/java/ru/geekbrains/spring/application/core/ConsoleAppConfiguration.java
+++ src/main/java/ru/geekbrains/spring/application/core/ConsoleAppConfiguration.java
@@ -2,6 +2,7 @@ package ru.geekbrains.spring.application.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.geekbrains.spring.application.core.orderRepository.OrderRepository;
 
 @Configuration
 public class ConsoleAppConfiguration {
@@ -18,6 +19,10 @@ public class ConsoleAppConfiguration {
 
     public AppProcessor AppProcessor(Cart cart, ProductRepository productRepository){
         return new AppProcessor(cart, productRepository);
+    }
+    @Bean
+    public OrderRepository orderRepository(){
+        return new OrderRepository();
     }
 
 }
